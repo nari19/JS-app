38f7686f091b3b9c7fd7e816bfa17fda3fc01059
narizuka-kazuki <30364686+narizuka-kazuki@users.noreply.github.com>
2018-02-03 16:48:18 +0900

    JSファイル追加

---
+++ switch.js
@@ -0,0 +1,165 @@
+switch(list_result[0]){
+					case 0:
+						$("#ranking01 img").attr({'src':'../mobile/result_num_1.png'});
+						break;
+					case 1:
+						$("#ranking01 img").attr({'src':'../mobile/result_num_2.png'});
+						break;
+					case 2:
+						$("#ranking01 img").attr({'src':'../mobile/result_num_3.png'});
+						break;
+					case 3:
+						$("#ranking01 img").attr({'src':'../mobile/result_num_4.png'});
+						break;
+					case 4:
+						$("#ranking01 img").attr({'src':'../mobile/result_num_5.png'});
+						break;
+					case 5:
+						$("#ranking01 img").attr({'src':'../mobile/result_num_6.png'});
+						break;
+				}
+switch(list_result[1]){
+					case 0:
+						$("#ranking02 img").attr({'src':'../mobile/result_num_1.png'});
+						break;
+					case 1:
+						$("#ranking02 img").attr({'src':'../mobile/result_num_2.png'});
+						break;
+					case 2:
+						$("#ranking02 img").attr({'src':'../mobile/result_num_3.png'});
+						break;
+					case 3:
+						$("#ranking02 img").attr({'src':'../mobile/result_num_4.png'});
+						break;
+					case 4:
+						$("#ranking02 img").attr({'src':'../mobile/result_num_5.png'});
+						break;
+					case 5:
+						$("#ranking02 img").attr({'src':'../mobile/result_num_6.png'});
+						break;
+					
+				}
+switch(list_result[2]){
+					case 0:
+						$("#ranking03 img").attr({'src':'../mobile/result_num_1.png'});
+						break;
+					case 1:
+						$("#ranking03 img").attr({'src':'../mobile/result_num_2.png'});
+						break;
+					case 2:
+						$("#ranking03 img").attr({'src':'../mobile/result_num_3.png'});
+						break;
+					case 3:
+						$("#ranking03 img").attr({'src':'../mobile/result_num_4.png'});
+						break;
+					case 4:
+						$("#ranking03 img").attr({'src':'../mobile/result_num_5.png'});
+						break;
+					case 5:
+						$("#ranking03 img").attr({'src':'../mobile/result_num_6.png'});
+						break;
+				}
+switch(list_result[3]){
+					case 0:
+						$("#ranking04 img").attr({'src':'../mobile/result_num_1.png'});
+						break;
+					case 1:
+						$("#ranking04 img").attr({'src':'../mobile/result_num_2.png'});
+						break;
+					case 2:
+						$("#ranking04 img").attr({'src':'../mobile/result_num_3.png'});
+						break;
+					case 3:
+						$("#ranking04 img").attr({'src':'../mobile/result_num_4.png'});
+						break;
+					case 4:
+						$("#ranking04 img").attr({'src':'../mobile/result_num_5.png'});
+						break;
+					case 5:
+						$("#ranking04 img").attr({'src':'../mobile/result_num_6.png'});
+						break;
+				}
+switch(list_result[4]){
+					case 0:
+						$("#ranking05 img").attr({'src':'../mobile/result_num_1.png'});
+						break;
+					case 1:
+						$("#ranking05 img").attr({'src':'../mobile/result_num_2.png'});
+						break;
+					case 2:
+						$("#ranking05 img").attr({'src':'../mobile/result_num_3.png'});
+						break;
+					case 3:
+						$("#ranking05 img").attr({'src':'../mobile/result_num_4.png'});
+						break;
+					case 4:
+						$("#ranking05 img").attr({'src':'../mobile/result_num_5.png'});
+						break;
+					case 5:
+						$("#ranking05 img").attr({'src':'../mobile/result_num_6.png'});
+						break;
+				}
+switch(list_result[5]){
+					case 0:
+						$("#ranking06 img").attr({'src':'../mobile/result_num_1.png'});
+						break;
+					case 1:
+						$("#ranking06 img").attr({'src':'../mobile/result_num_2.png'});
+						break;
+					case 2:
+						$("#ranking06 img").attr({'src':'../mobile/result_num_3.png'});
+						break;
+					case 3:
+						$("#ranking06 img").attr({'src':'../mobile/result_num_4.png'});
+						break;
+					case 4:
+						$("#ranking06 img").attr({'src':'../mobile/result_num_5.png'});
+						break;
+					case 5:
+						$("#ranking06 img").attr({'src':'../mobile/result_num_6.png'});
+						break;
+				}
+
+
+
+
+switch(list-result.indexOf('select2')){
+					case 0:
+						$("#result_num img").attr({'src':'../mobile/result_area_bar_1.png'});
+						break;
+					case 1:
+						$("#result_num img").attr({'src':'../mobile/result_area_bar_2.png'});
+						break;
+					case 2:
+						$("#result_num img").attr({'src':'../mobile/result_area_bar_3.png'});
+						break;
+					case 3:
+						$("#result_num img").attr({'src':'../mobile/result_area_bar_4.png'});
+						break;
+					case 4:
+						$("#result_num img").attr({'src':'../mobile/result_area_bar_5.png'});
+						break;
+					case 5:
+						$("#result_num img").attr({'src':'../mobile/result_area_bar_6.png'});
+						break;
+				}
+switch(select2){
+					case 0:
+						$("#result_name img").attr({'src':'../mobile/result_name_1.png'});
+						break;
+					case 1:
+						$("#result_name img").attr({'src':'../mobile/result_name_2.png'});
+						break;
+					case 2:
+						$("#result_name img").attr({'src':'../mobile/result_name_3.png'});
+						break;
+					case 3:
+						$("#result_name img").attr({'src':'../mobile/result_name_4.png'});
+						break;
+					case 4:
+						$("#result_name img").attr({'src':'../mobile/result_name_5.png'});
+						break;
+					case 5:
+						$("#result_name img").attr({'src':'../mobile/result_name_6.png'});
+						break;
+				}
